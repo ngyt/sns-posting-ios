e720a3bd987d289b5039674bb66146ececeb01d3
Angie <angie@qoncept.co.jp>
2016-06-29 11:20:45 +0900

    Share with Instagram.

--- Project/SnsPosting/ViewControllers/InstagramServiceViewController.swift
+++ Project/SnsPosting/ViewControllers/InstagramServiceViewController.swift
@@ -3,6 +3,9 @@ import UIKit
 class InstagramServiceViewController: BasicViewController {
     
     private let instagramUrlString = "instagram://app"
+    private let imageFileName = "share.igo"
+    private let instagramUTI = "com.instagram.exclusivegram"
+    private let documentController = UIDocumentInteractionController()
 
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
@@ -13,10 +16,24 @@ class InstagramServiceViewController: BasicViewController {
         }
         
         if UIApplication.sharedApplication().canOpenURL(instagramUrl) {
-            print("Found")
+            saveImageToLocale()
         } else {
-            print("Nooooooooo")
+            showAlert(title: "Fail tp open Instagram", message: "Could not open Instagram for sharing.")
         }
     }
 
+    func saveImageToLocale() {
+        guard let data = UIImageJPEGRepresentation(image, 1.0) else {
+            return
+        }
+        
+        let filePath = NSTemporaryDirectory().stringByAppendingString(imageFileName)
+        print(filePath)
+        data.writeToFile(filePath, atomically: true)
+        
+        let imageUrl = NSURL(fileURLWithPath: filePath)
+        documentController.URL = imageUrl
+        documentController.UTI = instagramUTI
+        documentController.presentOpenInMenuFromRect(CGRectMake(0, 0, 100, 100), inView: view, animated: true)
+    }
 }
