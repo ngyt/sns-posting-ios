54f65e6b0e42bc9bbd8f67a0c5335e77eb025fac
Angie <angie@qoncept.co.jp>
2016-06-29 10:18:41 +0900

    Implement ShareActivityViewController.

--- Project/SnsPosting/ViewControllers/ShareActivityViewController.swift
+++ Project/SnsPosting/ViewControllers/ShareActivityViewController.swift
@@ -2,26 +2,62 @@ import UIKit
 
 class ShareActivityViewController: BasicViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private let message = "Share message."
+    private let urlString = "https://www.google.co.jp"
+    private var image: UIImage!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var shareButton: UIButton!
+    @IBOutlet weak var messageSwitch: UISwitch!
+    @IBOutlet weak var urlSwitch: UISwitch!
+    @IBOutlet weak var imageSwitch: UISwitch!
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if let img = UIImage(named: "SharePhoto") {
+            image = img
+            imageView.image = image
+        } else {
+            let alert = UIAlertController(title: "Error", message: "No photo for testing.", preferredStyle: .Alert)
+            presentViewController(alert, animated: true, completion: nil)
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func onShareButton(sender: AnyObject) {
+        var items = [AnyObject]()
+        if messageSwitch.on {
+            items.append(message)
+        }
+        
+        if urlSwitch.on {
+            items.append(urlString)
+        }
+        
+        if imageSwitch.on {
+            items.append(image)
+        }
+        
+        if NSProcessInfo().isOperatingSystemAtLeastVersion(NSOperatingSystemVersion(majorVersion: 9, minorVersion: 0, patchVersion: 0)) {
+            share(items, activities: [])
+        } else {
+            share(items, activities: [LineActivity()])
+        }
     }
-    */
-
-}
+    
+    private func share(items: [AnyObject], activities: [UIActivity]?) {
+        let activity = UIActivityViewController(activityItems: items, applicationActivities: activities)
+        activity.excludedActivityTypes = [
+            UIActivityTypePostToWeibo,
+            UIActivityTypeMessage,
+            UIActivityTypePrint,
+            UIActivityTypeCopyToPasteboard,
+            UIActivityTypeAssignToContact,
+            UIActivityTypeSaveToCameraRoll,
+            UIActivityTypeAddToReadingList,
+            UIActivityTypePostToFlickr,
+            UIActivityTypePostToVimeo,
+            UIActivityTypePostToTencentWeibo,
+            UIActivityTypeAirDrop]
+        self.presentViewController(activity, animated: true, completion: nil)
+    }}
